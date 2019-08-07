07cd76fd06d4b3fed2a465a5634dfc10da4bccfb
WilliamVernaschi <53841356+WilliamVernaschi@users.noreply.github.com>
2019-08-06 22:04:41 -0300

    Update prntscrenn.py

--- prntscrenn.py
+++ prntscrenn.py
@@ -3,17 +3,18 @@ from string import ascii_lowercase
 from time import sleep
 from webbrowser import open
 
+linkAmount = 0
 urlCharacters = list()
 letters = ascii_lowercase
 goback = True
 while goback is True:
-    quanturl = (input('How many links do you wants?'))
-    if quanturl.isnumeric():
+    linkAmount = (input('How many links do you wants?'))
+    if linkAmount.isnumeric():
         break
     else:
         print('\033[31mPlease, only type in numbers.\033[m')
         goback = True
-for c in range(0, int(quanturl)):
+for c in range(0, int(linkAmount)):
     for cont in range(0, 5):
         chosenLetter = choice(letters)
         urlCharacters.append(chosenLetter)
